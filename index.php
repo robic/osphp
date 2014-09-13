27703eeef4ee902f5dd4e444985e561ff731da2d
robic <robicser@gmail.com>
2014-09-14 00:47:03 +0430

    initial
    initial

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php echo 'db_host: ' . $OPENSHIFT_DB_HOST;?>
+<?php echo '<br/>db_port: ' .$OPENSHIFT_DB_PORT;?>
+<?php echo '<br/>db_u: ' .$OPENSHIFT_DB_USERNAME;?>
+<?php echo '<br/>db_p: ' .$OPENSHIFT_DB_PASSWORD;?>
