020c5ae3d2ca267ecc072419b7d4177fd5a35341
robic <robicser@gmail.com>
2014-09-14 00:52:54 +0430

    correction

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-<?php echo 'db_host: ' . $OPENSHIFT_DB_HOST;?>
-<?php echo '<br/>db_port: ' .$OPENSHIFT_DB_PORT;?>
-<?php echo '<br/>db_u: ' .$OPENSHIFT_DB_USERNAME;?>
-<?php echo '<br/>db_p: ' .$OPENSHIFT_DB_PASSWORD;?>
+<?php echo "$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT";
